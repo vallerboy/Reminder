db30db692b56a28888ac89ec54ad724d4e2f6d0c
Oskix <oskar.polak@akademiakodu.pl>
2018-07-20 15:40:49 +0200

    Changed file separator, and moved it to Config class

---
+++ src/main/java/pl/oskarpolak/reminder/models/ConfigModel.java
@@ -0,0 +1,8 @@
+package pl.oskarpolak.reminder.models;
+
+public class ConfigModel {
+    public static String PATH_TO_FILE_ACCOUNT = "C:\\asd\\accounts.txt";
+    public static String PATH_TO_FILE_TASKS = "C:\\asd\\tasks.txt";
+    public static String DATE_FORMAT = "yyyy-MM-dd";
+    public static String IN_FILE_SEPARATOR = ":";
+}
