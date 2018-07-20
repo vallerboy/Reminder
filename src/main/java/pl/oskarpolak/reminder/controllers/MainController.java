e2888d82e591a97c952d8664f6a9bbd720293609
Oskix <oskar.polak@akademiakodu.pl>
2018-07-20 16:03:15 +0200

    Added exit command to MainMenu

--- src/main/java/pl/oskarpolak/reminder/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/reminder/controllers/MainController.java
@@ -44,6 +44,9 @@ public class MainController {
                       makeTaskDone();
                       break;
                 }
+                case "3": {
+                      System.exit(0);
+                }
                 default: {
                     mainView.showCommandNotExist();
                 }
